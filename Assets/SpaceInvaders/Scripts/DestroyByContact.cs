1e689d5705077031a1d9efb9c5f431cb9cc2721a
=ionutdejeu <=ionut.deju@gmail.com>
2015-12-01 14:03:25 +0200

    A

--- Assets/SpaceInvaders/Scripts/DestroyByContact.cs
+++ Assets/SpaceInvaders/Scripts/DestroyByContact.cs
@@ -1,10 +1,34 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class DestroyByContact : MonoBehaviour
 {
     public GameObject explosion;
     public GameObject playerExplosion;
+    public int scoreValue;
+    private  GameController gameController;
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+
+
+    void Start()
+    {
+
+        GameObject gameControllerObject = GameObject.FindWithTag("GameController");
+        if(gameControllerObject != null)
+        {
+            gameController = gameControllerObject.GetComponent<GameController>();
+        }
+        if(gameControllerObject == null)
+        {
+            Debug.Log("Cannot find  'GameController' script");
+        }
+        currentHealth = startingHealth;
+    }
     void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Boundary")
@@ -15,8 +39,12 @@ public class DestroyByContact : MonoBehaviour
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+            gameController.GameOver();
         }
-        Destroy(other.gameObject);
+        gameController.AddScore (scoreValue);
+        // Destroy(other.gameObject);   
+
+      
         Destroy(gameObject);
     }
 }
---
+++ Assets/SpaceInvaders/Scripts/HealthWall.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HealthWall : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+    public Image damageImage;
+    void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+
+
+}
+
