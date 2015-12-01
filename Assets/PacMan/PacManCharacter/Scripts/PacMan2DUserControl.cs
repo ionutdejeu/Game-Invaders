e1b6895409bc334c9d37dd255aec506e87fda8ba
=ionutdejeu <=ionut.deju@gmail.com>
2015-12-01 12:54:19 +0200

    Added fonts Animations of the transition in between the levels

---
+++ Assets/PacMan/PacManCharacter/Scripts/PacMan2DUserControl.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof (PacManCharacter2D))]
+public class PacMan2DUserControl : MonoBehaviour
+{
+    private PacManCharacter2D character;
+    private bool jump;
+
+    private void Awake()
+    {
+        character = GetComponent<PacManCharacter2D>();
+    }
+
+    private void Update()
+    {
+        if(!jump)
+        // Read the jump input in Update so button presses aren't missed.
+        jump = CrossPlatformInputManager.GetButtonDown("Jump");
+    }
+
+    private void FixedUpdate()
+    {
+        // Read the inputs.
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        // Pass all parameters to the character control script.
+        character.Move(h, crouch, jump);
+        jump = false;
+    }
+}
