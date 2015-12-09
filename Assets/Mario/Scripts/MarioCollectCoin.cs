2b2c407f13de2277406c58657993f55c3d9e0d86
AndreeaBrad <andreeabrad65@yahoo.com>
2015-12-09 20:57:28 +0200

    Andreea

---
+++ Assets/Mario/Scripts/MarioCollectCoin.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MarioCollectCoin : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        switch (collider.gameObject.name)
+        {
+            case "PacmanPrefab":
+            CoinController.coinCount++;
+                Destroy(this.gameObject);
+
+            break;
+        }
+
+    }
+}
